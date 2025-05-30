5ef725b03fdb764b06a0b784eebb6f5db58e5067
Kandankulathil <vinivincent2001@student.sfit.ac.in>
2025-05-30 07:17:32 +0530

    minor changes

--- kepaqmis/src/pages/QM/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import purchaseReducer from './pages/QM/purchaseSlice'; // adjust path if needed
-
-export const store = configureStore({
-  reducer: {
-    purchase: purchaseReducer,
-  },
-});
